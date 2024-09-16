149f00286008afa84d2f0af16e61365de0f7c9f6
Michael Lazar <lazar.michael22@gmail.com>
2024-09-16 10:57:10 -0400

    Fix for broken sauce

--- ascii/textmode/clients.py
+++ ascii/textmode/clients.py
@@ -13,17 +13,21 @@ class SixteenColorsClient:
         self.session = BaseSession(request_delay=1)
 
     def get_pack(self, name: str) -> dict:
-        resp = self.session.get(
-            f"{self.BASE_API}/pack/{name}",
-            params={
-                "archive": "true",
-                "content": "true",
-                "groups": "true",
-                "artists": "true",
-                "fileid": "true",
-                "sauce": "true",
-            },
-        )
+        params = {
+            "archive": "true",
+            "content": "true",
+            "groups": "true",
+            "artists": "true",
+            "fileid": "true",
+            "sauce": "true",
+        }
+
+        resp = self.session.get(f"{self.BASE_API}/pack/{name}", params=params)
+        if not resp.content:
+            # https://github.com/16colo-rs/16c/issues/93
+            params.pop("sauce")
+            resp = self.session.get(f"{self.BASE_API}/pack/{name}", params=params)
+
         return resp.json()["results"][0]
 
     def get_year(self, year: int) -> list[dict]:
