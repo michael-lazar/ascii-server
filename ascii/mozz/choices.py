92455d3aaad73e7476a5d17771c0107913d94ca2
Michael Lazar <lazar.michael22@gmail.com>
2025-11-02 19:08:10 -0500

    Add choices

--- ascii/mozz/choices.py
+++ ascii/mozz/choices.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class ArtPostFileType(models.TextChoices):
     TEXT = "text", "Plain text"
+    ANS = "ans", "Ans"
+    ASC = "asc", "Asc"
     XBIN = "xbin", "XBin"
     OTHER = "other", "Other"
 
@@ -11,4 +13,5 @@ class ArtPostFontName(models.TextChoices):
     MENLO = "menlo", "Menlo"
     TOPAZ_2PLUS = "topaz_2plus", "Amiga Topaz 2+"
     JGS = "jgs", "Jgs"
+    CP437 = "cp437", "IBM CP437"
     CUSTOM = "custom", "Custom"
