b7ce507feea5dac10a44443ae789a30f560b8e4c
Michael Lazar <lazar.michael22@gmail.com>
2025-11-22 22:25:36 -0500

    Typo

--- ascii/core/backends/storage.py
+++ ascii/core/backends/storage.py
@@ -9,7 +9,7 @@ class OverwriteFileSystemStorage(FileSystemStorage):
     def _save(self, name, content):
         if self.exists(name):
             self.delete(name)
-        return super().save(name, content)
+        return super()._save(name, content)
 
     def get_available_name(self, name, max_length=None):
         return name
